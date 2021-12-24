310fab4ee5c697b2e3af05a789af4195737e77e9
axxe.eth <maxime.brugel@gmail.com>
2021-12-24 10:11:49 +0100

    doc: add contract_size doc

--- scripts/contract_size.go
+++ scripts/contract_size.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"dapptools-template/scripts/utils"
 	"os"
 	"strconv"
-	"dapptools-template/scripts/utils"
 	"github.com/fatih/color"
 )
 
+// Command to get the contract size of a smart contract
+// Takes two arguments :
+// PATH : The smart contract path (for example : src/Greeter.sol)
+// NAME : The contract name (for example : Greeter)
 func main() {
 	if len(os.Args) != 3 {
 		color.Red("Wrong args... need contract path and name")
@@ -16,9 +20,11 @@ func main() {
 	color.Cyan("%s bytes", strconv.Itoa(size))
 
 	sizeLimit := 24576
-	if sizeLimit > size {	
-		color.Yellow("%s bytes left to reach the smart contract size limit of 24576 bytes.", strconv.Itoa(sizeLimit - size))
+	if sizeLimit > size {
+		color.Yellow("%s bytes left to reach the smart contract size limit of 24576 bytes.",
+			strconv.Itoa(sizeLimit-size))
 	} else {
-		color.Yellow("%s bytes more than the smart contract size limit of 24576 bytes.", strconv.Itoa(size - sizeLimit))
+		color.Yellow("%s bytes more than the smart contract size limit of 24576 bytes.", 
+			strconv.Itoa(size-sizeLimit))
 	}
 }
